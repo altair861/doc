207e2375fc7d452676edc8d5e89bd506c4d8a127
Chen Ye[陈烨] <cy8166@closeli.cn>
2019-10-30 10:56:43 +0800

    create

---
+++ publish.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+set -e
+#cd Hadoop-The-Definitive-Guide-4th &&\
+git checkout master &&\
+gitbook init &&\
+gitbook build &&\
+git add . &&\
+git commit -m 'update gitbook' &&\
+git push origin master &&\
+git checkout gh-pages &&\
+rm -rf * &&\
+git checkout master -- _book &&\
+mv _book/* ./ &&\
+rm -rf _book &&\
+rm -rf publish.sh &&\
+git add . &&\
+git commit -m 'publish gh-pages' &&\
+git push origin gh-pages &&\
+git checkout master

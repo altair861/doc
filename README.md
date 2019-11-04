553068b6bcbaff5582448d4ae0730bbc4c0e1739
Chen Ye[陈烨] <cy8166@closeli.cn>
2019-11-04 14:38:48 +0800

    plugin添加

--- README.md
+++ README.md
@@ -1,3 +1,23 @@
 [DBColumn注解](DBColumn/README.md)
 
 [IDEA-Plugin系列](idea_plugin/idea.md)
+
+
+
+
+
+
+
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/gitalk@1/dist/gitalk.css">
+<script src="https://cdn.jsdelivr.net/npm/gitalk@1/dist/gitalk.min.js"></script>
+<div id="gitalk-container"></div>
+var gitalk = new Gitalk({
+"clientID": "f452f976595591ff3304",
+"clientSecret": "230e5db6dfa1fdbe9a0e9987b58025092f938e62",
+"repo": "git@github.com:altair861/doc.git",
+"owner": "altair861",
+"admin": ["altair861"],
+"id": location.pathname,      
+"distractionFreeMode": false  
+});
+gitalk.render("gitalk-container");
--- publish.sh
+++ publish.sh
@@ -5,11 +5,25 @@ set -e
 cd `dirname $0`
 INDEX_HTML=./_book/index.html
 
+function getLine() {
+    str=$1
+    path=$2
+    grep -n ${str} ${path} | cut -d ":" -f 1
+}
+
+function delGitBookItem() {
+    count=$(getLine "www.gitbook.com" $INDEX_HTML)
+	echo "find in:${count}"
+	sed -i "" "`expr $count - 1`,`expr $count + 3`d" $INDEX_HTML
+}
+
+
+
 git checkout master &&\
 gitbook init &&\
 gitbook build &&\
 #去除目录里边的“Published with GitBook”项
-sed -i "" s/^.*www.gitbook.com.*$/" "/g ${INDEX_HTML}
+delGitBookItem
 git add . &&\
 git commit -m 'update gitbook' &&\
 git push origin master &&\
@@ -23,3 +37,4 @@ git add . &&\
 git commit -m 'publish gh-pages' &&\
 git push origin gh-pages &&\
 git checkout master
+
